c50eedb5358a0204ce82683d455058090d69a477
Yazan <186625651+yazanalkherret@users.noreply.github.com>
2026-02-10 14:25:04 +0300

    refactor: create base class for auth logic

---
+++ app/src/main/java/com/example/myapplication/viewmodels/BaseAuthViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+open class BaseAuthViewModel : ViewModel() {
+    protected val mAuth = FirebaseAuth.getInstance()
+    protected val db = FirebaseFirestore.getInstance()
+    protected val _message = MutableStateFlow<String?>(null)
+    val message: StateFlow<String?> = _message
+
+    fun clearMessage() {
+        _message.value = null
+    }
+}
